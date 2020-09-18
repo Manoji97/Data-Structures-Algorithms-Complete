693538046a925df3cbf933d219865376dd3a979c
manoji <mahamano1965@gmail.com>
2020-09-18 19:24:18 +0530

    Completed Sudoku.

--- InterviewQuestions/Python/7_BackTracking/2_SudokuSolver.py
+++ InterviewQuestions/Python/7_BackTracking/2_SudokuSolver.py
@@ -1,4 +1,19 @@
 '''
+
+Sudoku Solver 
+using back tracking
+
+steps:
+1) check if there is any empty slots
+2) if not ur puzlle is solved
+3) else pic the slot loop from 1 to 9, check for validity
+4) if valid recurse to next emt slot
+5) if no nums from 1 to 9 can solve that pos, recurse back and change and come back.
+
+if ans return true, else false
+
+but this not solving hard problems.
+
 '''
 
 
@@ -62,8 +77,7 @@ class Sudoku(object):
 		return True
 
 	def findSolution(self):
-		pos = self.findNextEmptyPos()
-		if self.solve(pos):
+		if self.solve():
 			self.printBoard()
 		else: print(False)
 
@@ -76,16 +90,18 @@ class Sudoku(object):
 
 		return False
 
-	def solve(self, pos):
+	def solve(self):
 		# Base Condition
-		if not self.findNextEmptyPos():
+		pos = self.findNextEmptyPos()
+		if not pos:
 			return True
+		self.printBoard()
+		print(' ')
 
 		for num in range(1, 10):
 			if self.checkValidity(num, pos):
 				self.board[pos[0]][pos[1]] = num
-				next_emt_pos = self.findNextEmptyPos()
-				if self.solve(next_emt_pos): return True
+				if self.solve(): return True
 				self.board[pos[0]][pos[1]] = 0
 
 		return False
@@ -121,8 +137,18 @@ _sudoku3 = [[0, 0, 6, 0, 5, 4, 9, 0, 0],
 		    [9, 0, 0, 8, 0, 0, 0, 5, 0],
 		    [0, 0, 0, 4, 0, 0, 3, 0, 7]]
 
+all_emt = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 3, 0, 8, 5],
+			[0, 0, 1, 0, 2, 0, 0, 0, 0],
+			[0, 0, 0, 5, 0, 7, 0, 0, 0],
+			[0, 0, 4, 0, 0, 0, 1, 0, 0],
+			[0, 9, 0, 0, 0, 0, 0, 0, 0],
+			[5, 0, 0, 0, 0, 0, 0, 7, 3],
+			[0, 0, 2, 0, 1, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 9]]
 
-sudokuSolver = Sudoku(_sudoku2)
+
+sudokuSolver = Sudoku(all_emt)
 
 sudokuSolver.printBoard()
 
